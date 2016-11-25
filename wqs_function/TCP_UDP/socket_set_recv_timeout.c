ef0590887fa87bd8f00116a4757667212c0e6125
wangqingsong <wangqingsong106@163.com>
2016-11-25 15:54:57 +0800

    test

--- wqs_function/TCP_UDP/socket_set_recv_timeout.c
+++ wqs_function/TCP_UDP/socket_set_recv_timeout.c
@@ -44,38 +44,47 @@ int main(int argc, char *argv[])
 
     fd_set rfds; //用于存放读的socket描述符
     struct timeval tv;  //设置select超时时间
-    int maxfd=server_fd;    //设置需要监听的最大的socket描述符中的最大值
+    int maxfd = server_fd;    //设置需要监听的最大的socket描述符中的最大值
 
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
-    FD_ZERO(&rfds); // 清空文件描述符集合
-    FD_SET( server_fd, &rfds);  // 将需要监听的socket描述符加入到集合中
-
-    ret = select(maxfd + 1, &rfds, NULL, NULL, &tv);    // 扫描集合，看看有哪个有数据传来，如果没有数据且设置了超时时间，超时后就会返回-1
-    switch(ret)
+    while( 1 )
     {
-    case -1:
-        perror("select error");
-        break;
-    case 0:
-        puts("select timeout");
-        break;
-    default:
-        memset(recv_buf, 0, sizeof(recv_buf));
-        ret = recvfrom(server_fd, recv_buf, N, 0, NULL, NULL);
-        if( 0 >= ret )
-        {
-            printf("recvfrom failed : %s\n", strerror(errno));
-            if( errno == EAGAIN )
-            {
-                puts("11");
-            }
-            return -1;
-        }
-        printf("recv from server : %s\n", recv_buf);
-        break;
-    }
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        /*清空文件描述符集合*/
+        FD_ZERO(&rfds);
+        /*将需要监听的socket描述符加入到集合中*/
+        FD_SET( server_fd, &rfds);
 
+        /*扫描集合，看看有哪个有数据传来，如果没有数据且设置了超时时间，超时后就会返回-1*/
+        ret = select(maxfd + 1, &rfds, NULL, NULL, &tv);
+        switch(ret)
+        {
+            case -1:
+                perror("select error");
+                break;
+            case 0:
+                puts("select timeout");
+                break;
+            default:
+                /*判断是否是server_fd有了消息*/
+                if( FD_ISSET(server_fd, &rfds) )
+                {
+                    memset(recv_buf, 0, sizeof(recv_buf));
+                    ret = recvfrom(server_fd, recv_buf, N, 0, NULL, NULL);
+                    if( 0 >= ret )
+                    {
+                        printf("recvfrom failed : %s\n", strerror(errno));
+                        if( errno == EAGAIN )
+                        {
+                            puts("11");
+                        }
+                        return -1;
+                    }
+                    printf("recv from server : %s\n", recv_buf);
+                }
+                break;
+        }
+    }
     close(server_fd);
 
     return 0;
