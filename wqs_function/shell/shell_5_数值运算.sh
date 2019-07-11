66385cecc26cc1c70040359d558d20b3e6b1065c
wangqingsong106@163.com <wangqingsong106@163.com>
2019-07-11 17:58:11 +0800

    a

--- wqs_function/shell/shell_5_数值运算.sh
+++ wqs_function/shell/shell_5_数值运算.sh
@@ -19,3 +19,10 @@ echo "$data1 / $data2 = $data"
 
 data=`expr $data1 % $data2`
 echo "$data1 % $data2 = $data"
+
+a=01
+b=10
+c=09
+# 当数值是以0开头时，bash默认是8进制数，所以当遇到这种数时，需要显示的为其声明为十进制，在变量或数值前加上"10#"即可
+((d=10#$a*60*60+10#$b*60+10#$c))
+echo "d=$d"
